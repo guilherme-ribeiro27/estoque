087f127bc2eae90e6d70ed23b9f4447fee621a17
guilherme-ribeiro27 <guilherme.ribeiro062002@gmail.com>
2023-08-11 18:28:13 -0300

    Initial configuration (swagger)

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { LikesModule } from './likes/likes.module';
+import { CommentsModule } from './comments/comments.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [UsersModule, PostsModule, LikesModule, CommentsModule, AuthModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { SwaggerModule, DocumentBuilder, SwaggerDocumentOptions } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  /// Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Estoque API')
+    .setDescription('Endpoints para API de estoque')
+    .setVersion('1.0')
+    .addTag('Auth')
+    .addTag('Users')
+    .addSecurityRequirements('Session', ['Bearer'])
+    .build();
+
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (
+      controllerKey: string,
+      methodKey: string
+    ) => methodKey
+  };
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3030);
+}
+bootstrap();
